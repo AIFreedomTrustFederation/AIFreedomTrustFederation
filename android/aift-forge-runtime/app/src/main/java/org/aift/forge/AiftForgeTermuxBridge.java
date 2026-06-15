c781dae0ad4804e4aa29f9d5ebb5f8a36721c1cc
AIFreedomTrustFederation <aifreedomtrust@gmail.com>
2026-06-15 09:13:53 -0700

    Add native Android runtime bridge stub

---
+++ android/aift-forge-runtime/app/src/main/java/org/aift/forge/AiftForgeTermuxBridge.java
@@ -0,0 +1,11 @@
+package org.aift.forge;
+
+import android.content.Context;
+
+public final class AiftForgeTermuxBridge {
+    private AiftForgeTermuxBridge() {}
+
+    public static boolean start(Context context) {
+        return false;
+    }
+}
